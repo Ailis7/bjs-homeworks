00786e78617aaca41f08089002b334d4545003b2
Petr Brovkin <bropet@mail.ru>
2020-01-30 16:48:48 +0300

    1-й Блок

---
+++ 1.1-variables/main.js
@@ -0,0 +1,14 @@
+let algebra = 4,
+  geography = 5,
+  physics = 4;
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+let myName = "Петр";
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+let x = 2;
+(y = 22), (z = 0);
+let result = x * y + 5 * z + x - 1;
+console.log(result);
