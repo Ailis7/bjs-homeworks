01d68f094b4a9efa5dcdea2128657772c27111c8
Petr Brovkin <bropet@mail.ru>
2020-02-03 21:13:05 +0300

    2.1 check

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,28 +1,49 @@
+"use strict";
 function calculateMortgage() {
-    let percent = window.percent.value;
-    let contribution = window.contribution.value;
-    let amount = window.amount.value;
-    let date = window.date.value;
+  let percent = window.percent.value;
+  let contribution = window.contribution.value;
+  let amount = window.amount.value;
+  let date = window.date.value;
 
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
-    let span = window.mortageResult;
-    span.textContent = result;
+  let result = calculateTotalMortgage(percent, contribution, amount, date);
+  let span = window.mortageResult;
+  span.textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    percent = parseFloat(percent);
+    contribution = parseFloat(contribution);
+    amount = parseFloat(amount);
+    let check = {"Процентная ставка": percent, "Начальный взнос": contribution, "Общая стоимость": amount};
+    for (let prop in check) {
+        if (check[prop] === Infinity || Number.isNaN(check[prop]) === true) {
+            return `Параметр ${prop} содержит неправильное значение ${check[prop]}`
+        }
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+
+
+  let totalAmount = percent + contribution + amount;
+  // код для задачи №1 писать здесь
+  return totalAmount;
 }
 
 function sayHello() {
-    let name = window.personName.value;
-    let greeting = getGreeting(name);
-    let span = window.helloResult;
-    span.textContent = greeting;
+  let name = window.personName.value;
+  let greeting = getGreeting(name);
+  let span = window.helloResult;
+  span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+  // код для задачи №2 писать здесь
+  //return greeting;
 }
+let arr = 11 / 0;
+if (arr == Infinity) {
+    console.log(`ghbdtn`)
+}
+console.log(isFinite(arr));
+console.log(arr);
+
+
