86304fcbea9a9e068699d29da1ffe8a0f3d5df6d
Petr Brovkin <[bropet@mail.ru]>
2020-02-05 15:50:27 +0300

    2.2 Final +-

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,31 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    }
+    this.sound = sound; 
+    return animal.sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let roundAverage = 0;
+    for (let i = 0; i < marks.length; i++) {
+        marks[i] = parseFloat(marks[i]);
+        roundAverage += marks[i];
+    }
+    let averageMark = Math.round(roundAverage / marks.length);
+    return averageMark;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    debugger;
+    let today = new Date();
+    let factBirthday = new Date(birthday);
+    let count = today - factBirthday;
+    if (count >= 567950400000) {  //568036800000 - по факту, но в 18-летие же все разрешено уже:) так что - 1 день.
+        console.log(count);
+        return count = "Да";
+    } else {
+        console.log("NO " + count);
+        return count = "Нет"; // не понял что нужно вывести в return, но в консоли всё выводится корретно. Возможно стоит переписать 6й пункт задания.
+    }
 }
