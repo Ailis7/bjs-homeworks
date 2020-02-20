dc90b47413e85e9c5112823716e7caf2e5584a09
Petr Brovkin <[bropet@mail.ru]>
2020-02-20 17:33:20 +0300

    3.1.5 final?

--- 3.1-array-proto-hocs/task.js
+++ 3.1-array-proto-hocs/task.js
@@ -6,28 +6,27 @@ function sleep(milliseconds) {
 
 function compareArrays(arr1, arr2) {
   function condition(element, index) {
-    //console.log(arr1[index], arr2[index], element);
     return arr1[index] === arr2[index];
   }
   return arr1.every(condition) && arr2.every(condition);
 }
 
-const sum = function sumary(a, b) {
-  return a + b;
+const sum = function sumary(...args) {
+  sleep(1500); // Можно использовать другое значение замедления.
+  return args.reduce((sum, arg) => {
+    return sum += +arg;
+  }, 0);
 };
 
 function memorize(fn, limit, results = []) {
-  //console.log(fn);
-  //console.log(limit)
 
   return function() {
-    let findIndex = results.length;
     if (results.find(arr => compareArrays(arr.arg, [...arguments]))) {
       return results[
         results.findIndex(arr => compareArrays(arr.arg, [...arguments]))
       ].result;
     }
-    if (results.length === 5) {
+    if (results.length === limit) {
       results.splice(0, 1);
     }
     results[results.length] = {
@@ -35,7 +34,6 @@ function memorize(fn, limit, results = []) {
       result: sum(...arguments)
     };
 
-    //console.log(limit);
     let a = JSON.stringify(results);
     console.table(a);
     return results;
@@ -51,29 +49,17 @@ mSum(1, 9);
 mSum(4, 7);
 mSum(8, 9);
 mSum(7, 8);
+mSum(1, 2);
 mSum(4, 7);
 
-// let a = [1, 2, 3, 4]
-// a.splice(0, 1);
 
-// //sleep(500); // Можно использовать другое значение замедления.
-// return limit.reduce((fn, limit) => {
-//     return fn += +limit;
-// }, 0);
-//}
-//console.log(mSum(100, 2, 3, 4, 6));
-
-// let a = {1: 9, 2: 3};
-// for (let prop in a) {
-//     console.log(a[prop])
-// }
-
-// let array = [-1, -2, 3, 4];
-// function nana(number) {
-//         if (number > 0) {
-//         return true;
-//     }
-//     return false;
-// }
-
-// console.log(array.find(array => nana(array)));
+let testArr = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
+function testCase(testFunction, procTime) {
+  console.time(testFunction);
+  array.forEach(element => {
+    
+  });
+};
+console.time(sum);
+console.log(sum(1, 2, 3));
+console.timeEnd(sum);
