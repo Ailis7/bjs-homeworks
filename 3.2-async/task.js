99c543fef5df751495184ab6220ac63e8171fd1a
Petr Brovkin <bropet@mail.ru>
2020-03-02 07:53:21 +0300

    1

--- 3.2-async/task.js
+++ 3.2-async/task.js
@@ -1,99 +1,144 @@
-'use strict'
+"use strict";
 class AlarmClock {
-    constructor() {
-        this.alarmCollection = [];
-        this.timerId = {};
-    }
+  constructor() {
+    this.alarmCollection = [];
+    this.timerId = {};
+  }
 
-    addClock(date, action, alarmID) {
-        if (alarmID === undefined) return console.error('ERROR - введите идентификатор будильника');
-        if (this.alarmCollection.find(identif => identif.ID === alarmID)) return console.error('Error - уже есть такое имя');
-        this.alarmCollection.push({
-            'ID': alarmID,
-            'time': date,
-            'callback': action
-        })
-    }
+  addClock(date, action, alarmID) {
+    if (alarmID === undefined)
+      return console.error("ERROR - введите идентификатор будильника");
+    if (this.alarmCollection.find(identif => identif.ID === alarmID))
+      return console.error("Error - уже есть такое имя");
+    this.alarmCollection.push({
+      ID: alarmID,
+      time: date,
+      callback: action
+    });
+  }
 
-    removeClock(alarmID) {
-        let deleteItem = this.alarmCollection.findIndex(element => element.ID === alarmID);
-        if (deleteItem === -1) return console.log(false);
-        this.alarmCollection.splice(deleteItem, ++deleteItem);
-        clearInterval(this.timerId[alarmID]);
-        return console.log(true);
-    }
+  removeClock(alarmID) {
+    let deleteItem = this.alarmCollection.findIndex(
+      element => element.ID === alarmID
+    );
+    if (deleteItem === -1) return console.log(false);
+    this.alarmCollection.splice(deleteItem, ++deleteItem);
+    clearInterval(this.timerId[alarmID]);
+    return console.log(true);
+  }
 
-    getCurrentFormattedTime() {
-        return (new Date().toLocaleTimeString('ru').split(':').splice(0, 2).join(':'));
-    }
+  getCurrentFormattedTime() {
+    return new Date()
+      .toLocaleTimeString("ru")
+      .split(":")
+      .splice(0, 2)
+      .join(":");
+  }
 
-    start() {
-        let checkClock = () => {
-            function transformDate(time) {
-                let dateArr = time.split(':');
-                return (Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), (dateArr[0] - 3), dateArr[1]) - new Date())
-            }
-            this.alarmCollection.forEach(element => this.timerId[element.ID] = setInterval(element.callback, transformDate(element.time)));
-        }
-        return checkClock();
-    }
+  start() {
+    let checkClock = alarm => {
+        console.log(alarm.time, abv.getCurrentFormattedTime())
+      if (alarm.time === abv.getCurrentFormattedTime()) {
+        this.timerId[alarm.ID] = setInterval(alarm.callback);
+        console.log(this.timerId[alarm.ID]);
+      }
+    };
 
-    stop() {
-        for (let prop in this.timerId) {
-            if (this.timerId[prop] !== null) {
-                clearInterval(this.timerId[prop]);
-                this.timerId[prop] = null;
-            }
-        }
-    }
+    setInterval(() => {
+        this.alarmCollection.forEach(element => checkClock(element))
+    }, 30000)
 
-    printAlarms() {
-        this.alarmCollection.forEach(element => console.log(`'${element.ID}' - ${element.time}`));
-    }
+    //     function transformDate(time) {
+    //         let dateArr = time.split(':');
+    //         return (Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), (dateArr[0] - 3), dateArr[1]) - new Date())
+    //     }
+    //     this.alarmCollection.forEach(element => this.timerId[element.ID] = setInterval(element.callback, transformDate(element.time)));
+    // }
+    // return checkClock();
+  }
 
-    clearAlarms() {
-        this.stop();
-        this.timerId = {};
-        this.alarmCollection = [];
+  stop() {
+    for (let prop in this.timerId) {
+      if (this.timerId[prop] !== null) {
+        clearInterval(this.timerId[prop]);
+        this.timerId[prop] = null;
+      }
     }
+  }
+
+  printAlarms() {
+    this.alarmCollection.forEach(element =>
+      console.log(`'${element.ID}' - ${element.time}`)
+    );
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.timerId = {};
+    this.alarmCollection = [];
+  }
 }
 
 function test() {
-    console.log('УРААА');
+  console.log("УРААА");
 }
 
 let abv = new AlarmClock();
 
 function testCase() {
-    abv.addClock(abv.getCurrentFormattedTime(), function () {
-        console.log('Hello world');
-    }, 'firstAlarm');
+  abv.addClock(
+    abv.getCurrentFormattedTime(),
+    function() {
+      console.log("Hello world");
+    },
+    "firstAlarm"
+  );
 
-    function plusMinute(n) {
-        let plus = abv.getCurrentFormattedTime().split(':')
-        plus[1] = parseFloat(plus[1]) + n;
-        return plus.join(':');
+  function plusMinute(n) {
+    let plus = abv.getCurrentFormattedTime().split(":");
+    plus[1] = parseFloat(plus[1]) + n;
+    if (plus[1].length < 2) {
+        plus[1] = "0" + plus[1]
     }
+    return plus.join(":");
+  }
 
-    abv.addClock(plusMinute(1), function () {
-        console.log('REMOVE world');
-        abv.removeClock('secondAlarm');
-    }, 'secondAlarm');
+  abv.addClock(
+    plusMinute(1),
+    function() {
+      console.log("REMOVE world");
+      abv.removeClock("secondAlarm");
+    },
+    "secondAlarm"
+  );
 
-    abv.addClock(plusMinute(2), function () {
-        console.log('Remove ALL alarms');
-        abv.stop();
-    }, 'thirdAlarm');
+  abv.addClock(
+    plusMinute(2),
+    function() {
+      console.log("Remove ALL alarms");
+      abv.stop();
+    },
+    "thirdAlarm"
+  );
 
-    abv.start();
+  abv.start();
 }
 
 testCase();
 
-let a = new Date('December 17, 1995 03:24:00').toLocaleTimeString("ru").split(":").splice(0, 2).join(":")
+let a = new Date("December 17, 1995 03:24:00")
+  .toLocaleTimeString("ru")
+  .split(":")
+  .splice(0, 2)
+  .join(":");
+
+
+
+//let b = 4;
+//console.log(Date().getMinutes(b))
 //a = a.toLocaleTimeString("ru").split(":");
 //a = a.splice(0, 2)
-console.log(a);
+//setInterval(test);
 
 // abv.addClock("16:16", "act");
 // abv.addClock("18:40", test, "Hi");
